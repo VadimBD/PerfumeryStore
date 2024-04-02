fd5dbd36c71bdab73ed3543918c6e5d912e4eea6
VadimBD <dreamvb5@gmail.com>
2024-04-02 16:41:19 +0300

    Added models

---
+++ PerfumeryStore/Models/Interfaces/IProductRepositorie.cs
@@ -0,0 +1,11 @@
+﻿namespace PerfumeryStore.Models.Interfaces
+{
+    public interface IProductRepositorie
+    {
+        IEnumerable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+        Product DeleteProduct(Product product);
+
+    }
+}
